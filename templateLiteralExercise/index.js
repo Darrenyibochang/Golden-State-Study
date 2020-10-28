f55c941b32dcb3e5ec207649b9d0622c3d86b8e7
Darrenyibochang <73253544+Darrenyibochang@users.noreply.github.com>
2020-10-27 23:01:50 -0700

    Add files via upload

---
+++ templateLiteralExercise/index.js
@@ -0,0 +1,8 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+var roll = parseInt(die1) + parseInt(die2)
+
+console.log(roll)
