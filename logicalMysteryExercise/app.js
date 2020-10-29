5b9e12a952ef106d7713e33f33b7720dece721a3
Darrenyibochang <73253544+Darrenyibochang@users.noreply.github.com>
2020-10-29 14:01:03 -0700

    Add files via upload

---
+++ logicalMysteryExercise/app.js
@@ -0,0 +1,8 @@
+const mystery = 'p123456'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if(mystery[0] === 'P2345678' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
+}
