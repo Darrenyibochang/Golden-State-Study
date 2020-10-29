d08c769648068ba5269d55c7e3cafc08fbe64f9e
Darrenyibochang <73253544+Darrenyibochang@users.noreply.github.com>
2020-10-29 13:43:01 -0700

    Add files via upload

---
+++ getColorConditionalExercise/app.js
@@ -0,0 +1,16 @@
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if(phrase =="stop"){
+        console.log("red")
+   }
+   if(phrase =="slow"){
+    console.log("yellow")
+    }
+    if(phrase =="go"){
+        console.log("green")
+    }
+   else{
+        console.log("purple")
+    }
+    //AND THIS LINE ↑↑↑↑↑
+}
