639766d9baf1dc7ae7982ff145828be62d12bb2e
Darrenyibochang <73253544+Darrenyibochang@users.noreply.github.com>
2020-10-30 10:12:14 -0700

    Add files via upload

---
+++ lterratingArray/app.js
@@ -0,0 +1,8 @@
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let i = 0; i<people.length; i++){
+  let name = people[i]
+  console.log(name.toUpperCase())
+}
